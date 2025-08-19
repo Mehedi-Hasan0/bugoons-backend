5e45c2c78ee74c385f781a8ecac39e9bef6552f8
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2025-08-19 12:05:14 +0600

    ADD: Routes, User routes added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,44 @@
 // src/index.ts
-import express, { Request, Response, Application } from 'express';
+import express, { Request, Response, Application, NextFunction } from 'express';
 import config from './config';
+import cors, { CorsOptions } from 'cors';
+import routes from './app/routes';
+import httpStatus from 'http-status';
 
 const app: Application = express();
 const port = config.port;
 
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    callback(null, origin || '*');
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// parse body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/api/v1', routes);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Bugoons Server');
+});
+
+// global error handler
+// app.use(globalErrorHandler);
+
+// not found routes
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not found',
+    errorMessage: [{ path: req.originalUrl, message: 'Api Not Found' }],
+  });
+  next();
 });
 
 app.listen(port, () => {
